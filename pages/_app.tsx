6f254f4cc1f1b365ffaffe371fcdf600a7d6274d
mikamada <mikamada17@gmail.com>
2023-09-21 15:49:08 +0700

    slicing ui sign-in page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import '../styles/utilities.css'
 import '../styles/homepage.css'
+import '../styles/detail.css'
+import '../styles/checkout.css'
+import '../styles/complete-checkout.css'
+import '../styles/sign-in.css'
+import '../styles/sign-up.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
