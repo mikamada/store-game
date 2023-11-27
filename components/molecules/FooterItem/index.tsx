670a9a161f768ab7ad2fac738c46684780578a9e
mikamada <mikamada17@gmail.com>
2023-11-27 15:13:19 +0700

    fix minor issue

--- components/molecules/FooterItem/index.tsx
+++ components/molecules/FooterItem/index.tsx
@@ -18,9 +18,10 @@ export default function FooterItem(props: Partial<FooterItemProps>) {
     <div className="col-md-4 col-6 mb-lg-0 mb-25">
       <p className="text-lg fw-semibold color-palette-1 mb-12">{title}</p>
       <ul className="list-unstyled">
-        {navLink?.map((item, i) => <li className="mb-6">
-          <Link key={i} href={`${item.href}`} className="text-lg color-palette-1 text-decoration-none">{item.name}</Link>
-        </li>)}
+        {navLink?.map((item, i) =>
+          <li className="mb-6" key={i}>
+            <Link href={`${item.href}`} className="text-lg color-palette-1 text-decoration-none">{item.name}</Link>
+          </li>)}
       </ul>
     </div>
   )
