19acfef187f35720e7daf700727c27baed16b5b7
mikamada <mikamada17@gmail.com>
2023-12-02 14:09:04 +0700

    refactor call api

---
+++ config/api/index.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+export const callAPI = async ({ url, method, data }: AxiosRequestConfig) => {
+	const response = await axios({
+		url,
+		method,
+		data,
+	}).catch((err) => err.response);
+
+	if (response.status > 300) {
+		const res = {
+			error: true,
+			message: response.data.message,
+			data: null,
+		};
+
+		return res;
+	}
+
+	const res = {
+		error: false,
+		message: response.data.message,
+		data: response.data.data,
+	};
+
+	return res;
+};
