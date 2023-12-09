6ab8678183019a7c54ec73a2346f01f5acbb9d07
mikamada <mikamada17@gmail.com>
2023-12-09 14:53:50 +0700

    add private route member transacrion

--- pages/member/transactions/index.tsx
+++ pages/member/transactions/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSidePropsContext } from "next";
 import Sidebar from "../../../components/organisms/Sidebar";
 import TransactionContent from "../../../components/organisms/TransactionContent";
+import { JwtPayloadTypes, UserTypes } from "../../../services/data-types";
+import { jwtDecode } from "jwt-decode";
 
 export default function Transaction() {
   return (
@@ -8,4 +11,28 @@ export default function Transaction() {
       <TransactionContent />
     </section>
   )
+}
+
+
+export const getServerSideProps = async ({ req }: GetServerSidePropsContext) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/sign-in",
+        permanent: false
+      }
+    }
+  }
+  const jwtToken = Buffer.from(token, "base64").toString("ascii");
+  const payload: JwtPayloadTypes = jwtDecode(jwtToken);
+  const userFromPayload: UserTypes = payload?.player;
+  const IMG = process.env.NEXT_PUBLIC_IMAGE;
+  userFromPayload.avatar = `${IMG}/${userFromPayload.avatar}`
+  return {
+    props: {
+      user: userFromPayload
+    }
+  }
+
 }
